fc9961065c23dd94bf08fe7302341e50ff2e5ad2
GuilhermeCorreaSouza25 <guilhermecorreasouza@gmail.com>
2019-10-01 17:34:20 -0300

    tp_final
    tp finalizado

--- tp1/testbench.v
+++ tp1/testbench.v
@@ -1,21 +1,22 @@
 `include "modulo.v"
 
 module testbench();
-  reg a,b,c,d,reset,ready;
+  reg a,b,c,d;
   wire s0,s1,s2,s3,s4,s5,s6;
 
-  Notas ex_instancia1(.a(a), .b(b), .c(c), .d(d), .s0(s0), .s1(s1), .s2(s2), .s3(s3), .s4(s4), .s5(s5), .s6(s6), .reset(reset), .ready(ready));
+  Notas ex_instancia1(.a(a), .b(b), .c(c), .d(d), .s0(s0), .s1(s1), .s2(s2), .s3(s3), .s4(s4), .s5(s5), .s6(s6));
 
   initial begin
-    a = 0;
-    b = 0;
-    c = 0;
-    d = 0;
+    a = 1;
+    b = 1;
+    c = 1;
+    d = 1;
 
     $dumpfile("ondas_xylo.vcd");
-    $dumpvars(0, testbench);
+    $dumpvars;
     $display("a b c d = s0 s1 s2 s3 s4 s5 s6");
-    $monitor("%b%b%b%b = %b %b %b %b %b %b %b ",a,b,c,d,s0,s1,s2,,s3,s4,s5,s6);
+    // $monitor("%b%b%b%b = %b %b %b %b %b %b %b ",a,b,c,d,s0,s1,s2,,s3,s4,s5,s6);
+    $monitor("%b%b%b%b = %b %b %b %b %b %b %b",a, b, c, d, s0, s1, s2, s3, s4, s5, s6);
 
 
     //always @ ( * ) begin
@@ -25,30 +26,26 @@ module testbench();
                //#1; clock = 4'b1;
                //#1; reset = 1'b1;
                //#1; reset = 1'b0;
-               reset = 0;
-               ready = 0;
-          #1;  reset = 1;
+               a = 0; b = 0; c = 0; d = 0;
+          #1;  a = 0; b = 0; c = 0; d = 1;
+          #1;  a = 0; b = 0; c = 1; d = 0;
+          #1;  a = 0; b = 0; c = 1; d = 1;
 
-          #1;  ready = 0;  a = 0; b = 0; c = 0; d = 0; ready = 1;
-          #1;  ready = 0;  a = 0; b = 0; c = 0; d = 1; ready = 1;
-          #1;  ready = 0;  a = 0; b = 0; c = 1; d = 0; ready = 1;
-          #1;  ready = 0;  a = 0; b = 0; c = 1; d = 1; ready = 1;
+          #1;  a = 0; b = 1; c = 0; d = 0;
+          #1;  a = 0; b = 1; c = 0; d = 1;
+          #1;  a = 0; b = 1; c = 1; d = 0;
+          #1;  a = 0; b = 1; c = 1; d = 1;
 
-          #1;  ready = 0;  a = 0; b = 1; c = 0; d = 0; ready = 1;
-          #1;  ready = 0;  a = 0; b = 1; c = 0; d = 1; ready = 1;
-          #1;  ready = 0;  a = 0; b = 1; c = 1; d = 0; ready = 1;
-          #1;  ready = 0;  a = 0; b = 1; c = 1; d = 1; ready = 1;
+          #1;  a = 1; b = 0; c = 0; d = 0;
+          #1;  a = 1; b = 0; c = 0; d = 1;
+          #1;  a = 1; b = 0; c = 1; d = 0;
+          #1;  a = 1; b = 0; c = 1; d = 1;
 
-          #1;  ready = 0;  a = 1; b = 0; c = 0; d = 0; ready = 1;
-          #1;  ready = 0;  a = 1; b = 0; c = 0; d = 1; ready = 1;
-          #1;  ready = 0;  a = 1; b = 0; c = 1; d = 0; ready = 1;
-          #1;  ready = 0;  a = 1; b = 0; c = 1; d = 1; ready = 1;
+          #1;  a = 1; b = 1; c = 0; d = 0;
+          #1;  a = 1; b = 1; c = 0; d = 1;
+          #1;  a = 1; b = 1; c = 1; d = 0;
+          #1;  a = 1; b = 1; c = 1; d = 1;
 
-          #1;  ready = 0;  a = 1; b = 1; c = 0; d = 0; ready = 1;
-          #1;  ready = 0;  a = 1; b = 1; c = 0; d = 1; ready = 1;
-          #1;  ready = 0;  a = 1; b = 1; c = 1; d = 0; ready = 1;
-          #1;  ready = 0;  a = 1; b = 1; c = 1; d = 1; ready = 1;
-
-           $finish;
+          #1 $finish;
   end
 endmodule
